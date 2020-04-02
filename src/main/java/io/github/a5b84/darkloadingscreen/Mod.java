5bb79e7bd9c654bcd99652a93c8dad9e3d64b794
Neecko <42934711+A5b84@users.noreply.github.com>
2020-04-02 13:30:14 +0200

    Point d'entrée renommé

--- src/main/java/io/github/a5b84/darkloadingscreen/Mod.java
+++ src/main/java/io/github/a5b84/darkloadingscreen/Mod.java
@@ -2,7 +2,7 @@ package io.github.a5b84.darkloadingscreen;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class DarkLoadingScreenMod implements ClientModInitializer {
+public class Mod implements ClientModInitializer {
 
     public static final String ID = "dark-loading-screen";
 
