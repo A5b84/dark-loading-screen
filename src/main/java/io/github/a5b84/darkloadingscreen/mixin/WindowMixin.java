d935ae33421e5eead2b1dd86188a05808cf58981
Neecko <42934711+A5b84@users.noreply.github.com>
2020-04-04 17:24:03 +0200

    Ajout mixin pour la couleur d'avant l'écran de chargement

---
+++ src/main/java/io/github/a5b84/darkloadingscreen/mixin/WindowMixin.java
@@ -0,0 +1,49 @@
+package io.github.a5b84.darkloadingscreen.mixin;
+
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GLCapabilities;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import io.github.a5b84.darkloadingscreen.Mod;
+import net.minecraft.client.util.Window;
+
+/**
+ * Mixin pour changer les couleurs de la fenêtre avant l'écran de chargement
+ */
+@Mixin(Window.class)
+public abstract class WindowMixin {
+
+    @Shadow private @Final long handle;
+
+    /** @see Window#Window */
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL;createCapabilities()Lorg/lwjgl/opengl/GLCapabilities;", remap = false), remap = false)
+    private GLCapabilities onCreateCapabilities() {
+        glfwSwapBuffers(handle); // Petit hack qui rend la fenêtre noire
+        //      le temps que `GL.createCapabilities()` se fasse
+        //      (vu qu'il prend quelques secondes)
+
+        final GLCapabilities result = GL.createCapabilities();
+
+        glClearColor(
+            (Mod.BG_COLOR & 0xff0000) / (float) 0xff0000,
+            (Mod.BG_COLOR & 0x00ff00) / (float) 0x00ff00,
+            (Mod.BG_COLOR & 0x0000ff) / (float) 0x0000ff,
+            1
+        );
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glfwSwapBuffers(handle);
+
+        return result;
+    }
+
+}
