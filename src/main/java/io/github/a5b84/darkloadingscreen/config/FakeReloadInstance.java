f534dfaa42ee8c479fe2df0a01c49d43e6564f6b
A5b84 <42934711+A5b84@users.noreply.github.com>
2026-06-19 21:01:59 +0200

    Make FakeReloadInstance safer

--- src/main/java/io/github/a5b84/darkloadingscreen/config/FakeReloadInstance.java
+++ src/main/java/io/github/a5b84/darkloadingscreen/config/FakeReloadInstance.java
@@ -1,37 +1,34 @@
 package io.github.a5b84.darkloadingscreen.config;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import net.minecraft.server.packs.resources.ReloadInstance;
 import net.minecraft.util.Mth;
-import net.minecraft.util.Unit;
 import net.minecraft.util.Util;
 
 /** {@link ReloadInstance} that automatically completes after some time */
 public class FakeReloadInstance implements ReloadInstance {
 
-  protected final long start;
-  protected final long duration;
+  private final long start;
+  private final long duration;
+  private final CompletableFuture<Void> future;
 
   public FakeReloadInstance(long durationMs) {
     start = Util.getMillis();
     duration = durationMs;
+    future =
+        CompletableFuture.runAsync(
+            () -> {}, CompletableFuture.delayedExecutor(durationMs, TimeUnit.MILLISECONDS));
   }
 
   @Override
-  public CompletableFuture<Unit> done() {
-    throw new UnsupportedOperationException();
+  public CompletableFuture<Void> done() {
+    return future;
   }
 
   @Override
   public float getActualProgress() {
-    return Mth.clamp((float) (Util.getMillis() - start) / duration, 0, 1);
+    float progress = (float) (Util.getMillis() - start) / duration;
+    return Mth.clamp(progress, 0, 1);
   }
-
-  @Override
-  public boolean isDone() {
-    return Util.getMillis() - start >= duration;
-  }
-
-  @Override
-  public void checkExceptions() {}
 }
